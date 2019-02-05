cdb78f5b68462c0291d36e8e0778b3677df5c350
SnoringBoy <34519954+SnoringBoy@users.noreply.github.com>
2019-02-05 10:43:03 -0800

    content: add animation to splash screen the  and login screen date: 2/5/2019 author:Joe Lu

--- app/src/main/java/edu/uw/chitchat/MainActivity.java
+++ app/src/main/java/edu/uw/chitchat/MainActivity.java
@@ -1,12 +1,16 @@
 package edu.uw.chitchat;
 
 import android.app.Activity;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnLoginFragmentInteractionListener {
+
+    private static final int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,22 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.frame_main_container, new SplashFragment())
                 .commit();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frame_main_container, new LoginFragment())
+                        .setCustomAnimations(R.anim.anim_fade_in, R.anim.anim_fade_out)
+                        .commit();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onLoginFragmentInteraction(Uri uri) {
+
     }
 }
--- app/src/main/java/edu/uw/chitchat/SplashFragment.java
+++ app/src/main/java/edu/uw/chitchat/SplashFragment.java
@@ -2,6 +2,7 @@ package edu.uw.chitchat;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +14,6 @@ import android.view.ViewGroup;
  */
 public class SplashFragment extends Fragment {
 
-
     public SplashFragment() {
         // Required empty public constructor
     }
@@ -22,6 +22,7 @@ public class SplashFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false);
     }
