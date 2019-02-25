e8942a90997db55f6166edcb3c1e0ee872f1586e
ljenny <ljenny@uw.edu>
2019-02-25 14:02:57 -0800

    added id to Chat

--- app/src/main/java/edu/uw/chitchat/chat/Chat.java
+++ app/src/main/java/edu/uw/chitchat/chat/Chat.java
@@ -6,14 +6,17 @@ public class Chat implements Serializable {
     private final String mName;
     private final String mDate;
     private final String mTeaser;
+    private final String mChatId;
 
-    public Chat(String name, String date, String teaser) {
+    public Chat(String name, String date, String teaser, String id) {
         this.mName = name;
         this.mDate = date;
         this.mTeaser = teaser;
+        this.mChatId = id;
     }
 
     public String getName() { return mName; }
     public String getDate() { return mDate; }
     public String getTeaser() { return mTeaser; }
+    public String getChatId() { return mChatId; }
 }
